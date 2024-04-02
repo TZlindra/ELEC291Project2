aa973a001677c4db969c86529919bab29ec1a856
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-04-01 17:46:22 -0700

    Nice Frequencies for Speaker

--- STM32/Controller/speaker.h
+++ STM32/Controller/speaker.h
@@ -2,7 +2,9 @@
 
 // #define TICK_FREQ_TIM2 4096L
 // #define TICK_FREQ_TIM2 5120L
-#define TICK_FREQ_TIM2 3520L
+// #define TICK_FREQ_TIM2 3520L
+// #define TICK_FREQ_TIM2 7040L
+#define TICK_FREQ_TIM2 14080L
 
 void InitTimer2(void);
 void ToggleSpeaker(void);
